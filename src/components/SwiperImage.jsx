95e50037d7f3aaca5984973ba6dcbfd688cb9e0f
MukhamedovK <kamoliddin2619@gmail.com>
2024-04-30 17:45:20 +0500

    fix response bugs in SwiperImage

--- src/components/SwiperImage.jsx
+++ src/components/SwiperImage.jsx
@@ -32,18 +32,18 @@ const SwiperImage = () => {
       }}
       navigation={true}
       modules={[Pagination, Navigation, Autoplay]}
-      className="bg-white w-[80%] h-[500px] rounded-[30px]"
+      className="bg-white w-[80%] rounded-[30px]"
     >
       {filterCards.map((item, id) => (
         <SwiperSlide key={id}>
           <div className="swiper-slide-content relative bg-gray-100 rounded-lg shadow-lg">
             <img className="w-auto h-[500px] m-auto" src={item.thumbnail} alt={item.title} />
             <div className="absolute bottom-0 top-0 right-0 left-0 p-4 bg-black bg-opacity-50">
-              <div className='text-center pt-[350px] text-white'>
+              <div className='text-center pt-[28%] text-white'>
                 <h2 className="text-lg font-bold mb-2">{item.title}</h2>
                 <p className="text-sm">{item.description}</p>
-                <p className="text-sm">Price: ${item.price}</p>
-                <p className="text-sm">Rating: {item.rating}</p>
+                <p className="text-sm pt-1">Price: ${item.price}</p>
+                <p className="text-sm pt-1">Rating: {item.rating}</p>
               </div>
             </div>
           </div>
